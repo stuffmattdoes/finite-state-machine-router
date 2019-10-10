317034b4fb19591c392c4e9a9419f4da455b13ca
Matthew J. Morrison <matt@ascentus.com>
2019-10-10 00:28:11 -0400

    url nav working

--- src/machine/Machine.jsx
+++ src/machine/Machine.jsx
@@ -6,29 +6,35 @@ export const StateMachineContext = React.createContext();
 StateMachineContext.displayName = 'Machine';
 
 export function Machine ({ children, history, id, url }) {
-    const [ state, setState ] = useState({ current: `#${id}` });
+    const [ state, setState ] = useState({
+        current: `#${id}`,
+        // derivedFromUrl: false
+    });
 
     // Default history
     if (!history) {
+        // console.log('history');
         history = createBrowserHistory({ basename: url });
     }
-
-    useEffect(() => history.listen(console.log), []);
     
     const matches = (stateId) => state.current.split('.').includes(stateId);
     const resolveStack = (stack) => {
-        console.log('resolveStack', stack);
+        console.log('resolveStack', stack, state);
         setState({ ...state, current: `#${id}${stack}` });
     }
     const resolveUrl = (url) => {
-        console.log('resolveUrl', url);
-        history.push(url, { state: state.current });
+        console.log('resolveUrl', url, state);
+        history.push(url, { stack: state.current });
     }
     const transition = (event, target) => {
         console.log('transition', event, target);
         // log(state, event, target);
         setState({ ...state, current: `#${id}.${target}` })
     }
+
+    useEffect(() => history.listen((location, action) => {
+        resolveStack(_routeMap[location.pathname]);
+    }));
 
     const routeMap = (childStates, parentUrl, parentStack) => {
         return childStates.reduce((acc, { props }, i) => {
@@ -48,27 +54,22 @@ export function Machine ({ children, history, id, url }) {
         }, {});
     }
 
-    // useEffect(() => {
-    //     const nextUrl = Object.keys(_routeMap).find(key => state.current === '#' + id + _routeMap[key]);
-    //     resolveUrl(nextUrl);
-    // }, [ state.current ]);
-
     // Determine initial child StateNode (if undefined, which is likely)
     const { _children, _routeMap } = useMemo(() => {
         let _childStates = React.Children.toArray(children).filter(c => c.type.name === 'State');
         const _hasInitialChild = _childStates.filter(c => c.props.initial).length > 0;
-
-        // 1. Derive state from URL
         const _pathname = history.location.pathname;
         const _routeMap = routeMap(_childStates);
 
         // 1. Derive state from URL
         if (_pathname.slice(1)) {
             if (_routeMap.hasOwnProperty(_pathname)) {
+                // setState({ ...state, derivedFromUrl: true });
                 resolveStack(_routeMap[_pathname]);
             } else {
                 // TODO:
                 // Resolve to 404
+                console.error('Route not found!');
             }
         }
         // 2. Resolve initial children
