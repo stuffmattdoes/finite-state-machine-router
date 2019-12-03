2514d4858c1decc1681a48a191eed2042a7355e4
Matt Morrison <m.james.morrison00@gmail.com>
2019-12-03 17:03:10 -0500

    cleanup the machine. yeet!

--- src/machine/Machine.jsx
+++ src/machine/Machine.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { createBrowserHistory } from 'history';
-import { deriveStateFromUrl, injectUrlParameters, isRootSemgent } from './util';
+import { deriveStateFromUrl, getChildStateNodes, getAllRoutes, getAllStacks, injectUrlParameters, isRootSemgent } from './util';
 
 export const MachineContext = React.createContext();
 MachineContext.displayName = 'Machine';
@@ -16,107 +16,52 @@ export function Machine ({ children: machineChildren, history, id: machineId, pa
         history = createBrowserHistory({ basename: machinePath });
     }
 
-    function resolveByStack(stack) {
-        console.log('resolveByStack', stack);
-        setState({ ...state, _event: null, current: stack });
-    }
+    // function resolvePath(path) {
+    //     const url = injectUrlParameters(path, urlParams);
 
-    function resolveByState(stateId) {
-        // console.log('resolveByState', stateId);
-        const stack = stacks.find(s =>  s.split('.').pop() === stateId);
-        resolveByStack(stack);
-    }
+    //     if (url !== history.location.pathname) {
+    //         console.log('resolvePath', history.location.pathname, 'to', url);
+    //         history.push(url, { stack: state.current });
+    //     }
+    // }
 
-    function resolvePath(path) {
-        const url = injectUrlParameters(path, urlParams);
-
-        if (url !== history.location.pathname) {
-            console.log('resolvePath', history.location.pathname, 'to', url);
-            history.push(url, { stack: state.current });
-        }
+    function resolveState(stateId) {
+        const stack = stacks.find(s => s.split('.').pop() === stateId);
+        setState({ ...state, current: stack });
     }
 
     function send(event, data = null) {
-        // console.log('send', event, data);
         setState({ ...state, _event: { name: event, ...data } });
     }
 
-    // For deriving effective state from URL
-    function generateStackMaps(states, parentPath, parentStack) {
-        return states.reduce((acc, child) => {
-            const { children, id, path } = child.props;
-            const grandChildStates = React.Children.toArray(children).filter(c => c.type.name === 'State');
-            const stackPath = parentPath ? parentPath + path : path;
-            const stack = parentStack ? `${parentStack}.${id}` : `#${machineId}.${id}`;
-
-            if (id) {
-                if (acc.stacks.includes(id)) {
-                    console.error(`State Machine already includes StateNode with an id of "${id}". All Id properties must be unique!`);
-                } else {
-                    acc.stacks.push(stack);
-                }
-            }
-            if (path) {
-                acc.routeMap[stackPath] = stack;
-            }
-            if (grandChildStates.length) {
-                const nextAcc = generateStackMaps(grandChildStates, stackPath, stack);
-                acc.routeMap = { ...acc.routeMap, ...nextAcc.routeMap };
-                acc.stacks = [ ...acc.stacks, ...nextAcc.stacks ];
-            }
-
-            return acc;
-        }, { routeMap: {}, stacks: [] });
+    function generateStackMaps(stateNodes, rootId) {
+        return {
+            routes: getAllRoutes(stateNodes),
+            stacks: getAllStacks(stateNodes).map(s => '#' + rootId + s)
+        }
     }
 
-    const { childStates, urlParams, routeMap, stacks } = useMemo(() => {
-        let childStates = React.Children.toArray(machineChildren).filter(c => c.type.name === 'State');
-        const { routeMap, stacks } = generateStackMaps(childStates);
-        const initialChild = childStates.find(c => c.props.initial) || childStates[0];
-        const { pathname: url } = history.location;
-        let urlParams = {};
-
-        // Derive state from URL
-        if (!isRootSemgent(url)) {
-            const match = deriveStateFromUrl(url, routeMap);
-            const { params, path, stack } = match;
-            urlParams = params;
-
-            if (match) {
-                resolveByStack(stack);
-            } else {
-                // Resolve to 404
-                resolveByStack(`#${machineId}.*`);
-            }
-        } else {
-            // Resolve to default URL
-            resolveByStack(`#${machineId}.${initialChild.props.id}`);
-        }
+    const { childStates, routes, stacks } = useMemo(() => {
+        let childStates = getChildStateNodes(machineChildren);
+        const { routes, stacks } = generateStackMaps(childStates, machineId);
 
         return {
             childStates,
-            urlParams,
-            routeMap,
+            routes,
             stacks
-        };
+        }
     }, [ machineChildren ]);
 
-    useEffect(() => history.listen((location, action) => {
-        const { params, path, stack } = deriveStateFromUrl(location.pathname, routeMap);
-
-        if (stack) {
-            resolveByStack(stack);
-        }
-    }));
+    useMemo(() => {
+        const initialChild = childStates.find(c => c.props.initial) || childStates[0];
+        resolveState(initialChild.props.id);
+    }, []);
 
     const providerValue = {
-        ...state,
+        current: state.current,
         history,
         id: machineId,
-        params: urlParams,
-        resolvePath,
-        resolveByStack,
-        resolveByState,
+        resolveState,
         send
     }
 
--- src/machine/util.js
+++ src/machine/util.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const logger = (state, event, target) => {
     const { current, id } = state;
     const date = new Date();
@@ -23,70 +25,112 @@ export const isDynamic = segment => paramRegExp.test(segment);
 export const isRootSemgent = url => url.slice(1) === '';
 export const segmentize = url => url.replace(/(^\/+|\/+$)/g, '').split('/');
 
-export const injectUrlParameters = (path, params) => {
-    const url = segmentize(path).map(seg => {
-        if (isDynamic(seg)) {
-            const segParam = seg.replace(':', '');
+// export const injectUrlParameters = (path, params) => {
+//     const url = segmentize(path).map(seg => {
+//         if (isDynamic(seg)) {
+//             const segParam = seg.replace(':', '');
 
-            if (Object.keys(params).includes(segParam)) {
-                return params[segParam];
-            } else {
-                console.error(`Cannot push to a dynamic URL without supplying the proper parameters: ${seg} parameter is missing.`);
-            }
-        }
+//             if (Object.keys(params).includes(segParam)) {
+//                 return params[segParam];
+//             } else {
+//                 console.error(`Cannot push to a dynamic URL without supplying the proper parameters: ${seg} parameter is missing.`);
+//             }
+//         }
 
-        return seg;
-    });
+//         return seg;
+//     });
 
-    return '/' + url.join('/');
+//     return '/' + url.join('/');
+// }
+
+// export const deriveStateFromUrl = (url, routeMap) => {
+//     let match = {
+//         params: {},
+//         path: url,
+//         stack: routeMap[url]
+//     }
+
+//     // 1. Exact match, no dynamic URL needed
+//     if (match.stack) {
+//         return match;
+//     }
+
+//     // 2. No exact match, check for dynamic URL match
+//     const dynamicPaths = Object.keys(routeMap).filter(route => route.match(/\/:/g));
+
+//     if (dynamicPaths.length) {
+//         // 2.1 Split url && route map into arrays, compare 1 by 1
+//         const urlSegments = segmentize(url);
+//         let params;
+//         const path = dynamicPaths.find(p => {
+//             const pathSegments = segmentize(p);
+//             params = {};
+
+//             if (pathSegments.length !== urlSegments.length) {
+//                 return false;
+//             }
+
+//             // 2.2 infer parameter from URL from first segment array that matches in length
+//             return !pathSegments.map((pathSegment, i) => {
+//                 if (isDynamic(pathSegment)) {
+//                     params[pathSegment.slice(1)] = urlSegments[i];
+//                     return true;
+//                 } else if (pathSegment === urlSegments[i]) {
+//                     return true;
+//                 }
+
+//                 return false;
+//             }).includes(false);
+//         });
+
+//         return {
+//             params,
+//             path,
+//             stack: routeMap[path]
+//         }
+//     } else {
+//         return null;
+//     }
+// }
+
+export function getChildStateNodes (children) {
+    return React.Children.toArray(children).filter(c => c.type.name === 'State');
 }
 
-export const deriveStateFromUrl = (url, routeMap) => {
-    let match = {
-        params: {},
-        path: url,
-        stack: routeMap[url]
-    }
+export function getAllStacks(stateNodes) {
+    return stateNodes.reduce((acc, child) => {
+        const childStates = getChildStateNodes(child.props.children);
+        const grandChildStacks = getAllStacks(childStates);
+        const { id } = child.props;
 
-    // 1. Exact match, no dynamic URL needed
-    if (match.stack) {
-        return match;
-    }
-
-    // 2. No exact match, check for dynamic URL match
-    const dynamicPaths = Object.keys(routeMap).filter(route => route.match(/\/:/g));
-
-    if (dynamicPaths.length) {
-        // 2.1 Split url && route map into arrays, compare 1 by 1
-        const urlSegments = segmentize(url);
-        let params;
-        const path = dynamicPaths.find(p => {
-            const pathSegments = segmentize(p);
-            params = {};
-
-            if (pathSegments.length !== urlSegments.length) {
-                return false;
-            }
-
-            // 2.2 infer parameter from URL from first segment array that matches in length
-            return !pathSegments.map((pathSegment, i) => {
-                if (isDynamic(pathSegment)) {
-                    params[pathSegment.slice(1)] = urlSegments[i];
-                    return true;
-                } else if (pathSegment === urlSegments[i]) {
-                    return true;
-                }
-
-                return false;
-            }).includes(false);
-        });
-
-        return {
-            params,
-            path,
-            stack: routeMap[path]
+        acc.push('.' + id);
+        
+        if (grandChildStacks.length) {
+            grandChildStacks.forEach(gcs => acc.push('.' + id + gcs));
         }
-    } else {
-        return null;
-    }
+
+        return acc;
+    }, []);
+}
+
+export function getAllRoutes(stateNodes) {
+    return stateNodes.reduce((acc, child) => {
+        const childStates = getChildStateNodes(child.props.children);
+        const grandChildRoutes = getAllRoutes(childStates);
+        const grandChildRoutesArr = Object.keys(grandChildRoutes);
+        const { id, path } = child.props;
+
+        if (path) {
+            acc[path] = '.' + id;
+        }
+        
+        if (grandChildRoutesArr.length) {
+            grandChildRoutesArr.forEach(route => acc[path ?
+                path + route
+                : route] = '.' + id + grandChildRoutes[route]
+            );
+        }
+
+        return acc;
+    }, {});
 }
