2da35ffc1fecabefc11ca1f8228a2f1478a73ec3
Matt Morrison <m.james.morrison00@gmail.com>
2020-06-17 12:23:53 -0400

    prep for publishing

---
+++ index.js
@@ -0,0 +1,13 @@
+import Link from './src/Link';
+import Machine, { createMachine, useMachine } from './src/Machine';
+import State from './src/State';
+import Transition from './src/Transition';
+
+export {
+    createMachine,
+    Link,
+    Machine,
+    State,
+    Transition,
+    useMachine
+}
