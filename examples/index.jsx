1fe24f2bf271612a5bb3799d296a3ff9a2de5d2d
Matt Morrison <m.james.morrison00@gmail.com>
2020-06-19 18:05:50 -0400

    WIP

--- examples/index.jsx
+++ examples/index.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link, Machine, State, Transition } from '../src';
-
-const App = ({ children }) => <main>
-    <header>Example | Finite State Machine Router</header>
-    {children}
-    <nav>
-        <Link className='custom-class' href='/parent/child'>Link</Link>
-        <Link className='custom-class' href='/parent/child-2'>Link</Link>
-    </nav>
-</main>;
-
-const Child = (props) => <div><h1>Child</h1></div>;
-const Error = (props) => <div><h1>Error</h1></div>
-const NotFound = (props) => <div><h1>Not Found</h1></div>
+import { App, Child, Error, NotFound } from './components';
 
 ReactDOM.render(
     <Machine id='home' path='/home'>
