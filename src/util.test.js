e7ec3fb5e4b0f5d9da718aee4771073398af0f44
Matt Morrison <m.james.morrison00@gmail.com>
2020-06-22 15:22:26 -0400

    test for util.js/normalizeChildStateProps

--- src/util.test.js
+++ src/util.test.js
@@ -3,10 +3,11 @@ import State from './State';
 import Transition from './Transition';
 import {
     // getChildrenOfType,
+    classNames,
     getChildStateNodes,
     getInitialChildStateNode,
     injectUrlParams,
-    isCurrentStack, 
+    isCurrentStack,
     isExactStack,
     normalizeChildStateProps,
     resolveInitial,
@@ -15,35 +16,46 @@ import {
 } from './util';
 
 describe('utility functions', () => {
-    // describe('getChildrenOfType', () => {
+    test('classNames', () => { 
+        const className = [
+            'custom-class',
+            { 'should-display': true },
+            { 'should-not-display': false }
+        ];
 
-    // });
+        const className2 = [
+            { 'should-not-display': false }
+        ];
+
+        expect (classNames(className)).toBe('custom-class should-display');
+        expect (classNames(className2)).toBeNull();
+    });
 
     test('getChildStateNodes', () => {
-        const components = <State id='parent'>
+        const machine = <State id='parent'>
             <State id='child-1'>
                 <Transition event='test-event-1' target='child-2'/>
             </State>
             <State id='child-2'/>
         </State>;
 
-        const childStates = getChildStateNodes(components).every(child => child.type.displayName === 'State');
+        const childStates = getChildStateNodes(machine).every(child => child.type.displayName === 'State');
         expect(childStates).toBe(true);
     });
 
     test('getInitialChildStateNode', () => {
-        const componentsWithoutInitial = [
+        const machineWithoutInitial = [
             <State id='child-1'/>,
             <State id='child-2'/>
         ];
 
-        const componentsWithInitial = [
+        const machineWithInitial = [
             <State id='child-1'/>,
             <State id='child-2' initial/>
         ];
 
-        const result1 = getInitialChildStateNode(getChildStateNodes(componentsWithoutInitial)).props.id;
-        const result2 = getInitialChildStateNode(getChildStateNodes(componentsWithInitial)).props.id;
+        const result1 = getInitialChildStateNode(getChildStateNodes(machineWithoutInitial)).props.id;
+        const result2 = getInitialChildStateNode(getChildStateNodes(machineWithInitial)).props.id;
 
         expect(result1).toBe('child-1');
         expect(result2).toBe('child-2');
@@ -69,27 +81,81 @@ describe('utility functions', () => {
         expect(injectUrlParams(path6, params)).toBe('/static-path/dynamic-path/another-static-path');
     });
 
-    // describe('isCurrentStack', () => {
+    // test('isCurrentStack', () => {
 
     // });
 
-    // describe('isExactStack', () => {
+    // test('isExactStack', () => {
 
     // });
 
-    // describe('normalizeChildStateProps', () => {
+    test('normalizeChildStateProps', () => {
+        const machine = React.Children.toArray(<State id='parent' path='/parent'>
+            <State id='child-1' path='/child-1'>
+                <Transition event='test-event' target='child-2'/>
+                <State id='grand-child'/>
+            </State>
+            <State id='child-2'/>
+        </State>);
+
+        const childStateNodes = [
+            {
+                childStates: [ 'child-1', 'child-2' ],
+                id: 'parent',
+                initial: true,
+                path: '/parent',
+                stack: '#home.parent',
+                transitions: [],
+                type: 'compound'
+            },
+            {
+                childStates: [ 'grand-child' ],
+                id: 'child-1',
+                initial: true,
+                path: '/parent/child-1',
+                stack: '#home.parent.child-1',
+                transitions: [
+                    {
+                        cond: null,
+                        event: 'test-event',
+                        sendId: 'child-1',
+                        target: 'child-2'
+                    }
+                ],
+                type: 'default'
+            },
+            {
+                childStates: [ 'grand-child' ],
+                id: 'grand-child',
+                initial: true,
+                path: '/parent/child-1',
+                stack: '#home.parent.child-1.grand-child',
+                transitions: [],
+                type: 'atomic'
+            },
+            {
+                childStates: [],
+                id: 'child-2',
+                initial: false,
+                path: '/parent',
+                stack: '#home.parent.child-2',
+                transitions: [],
+                type: 'atomic'
+            },
+        ];
+
+        expect(normalizeChildStateProps(machine, 'home').toString()).toBe(childStateNodes.toString());
+    });
+
+    // test('resolveInitial', () => {
 
     // });
 
-    // describe('resolveInitial', () => {
+    // test('resolveToAtomic', () => {
 
     // });
 
-    // describe('resolveToAtomic', () => {
-
-    // });
-
-    // describe('selectTransition', () => {
+    // test('selectTransition', () => {
 
     // });
 });
