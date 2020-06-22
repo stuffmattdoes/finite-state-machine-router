bf406288a186b5d950f6ad17ac631019929839c6
Matt Morrison <m.james.morrison00@gmail.com>
2020-06-22 11:53:45 -0400

    version update

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,20 @@ module.exports = {
         libraryTarget: 'umd',
         publicPath: '/dist/'
     },
+    externals: {
+        'react': {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react',
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom',
+        }
+    },
     resolve: {
         extensions: [ '.js', '.jsx' ],
     }
