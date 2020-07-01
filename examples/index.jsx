df2e01da17dcf91333d70ec30a9a5c5b2d6c18c0
Matt Morrison <m.james.morrison00@gmail.com>
2020-07-01 15:06:39 -0400

    Updated example

--- examples/index.jsx
+++ examples/index.jsx
@@ -10,10 +10,12 @@ const App = ({ children, machine }) => <main>
         <Link href='/parent/child-3'>URL: Child 3</Link><br/>
         <Link href='/parent/child-4'>URL: Child 4</Link>
     </nav>
-    {/* <ul>
-        <li onClick={event => machine.send('test-event-1')}>EVENT: Child 1</li>
-        <li onClick={event => machine.send('test-event-2')}>EVENT: Grand Child 2</li>
-    </ul> */}
+    <ul>
+        <li onClick={event => machine.send('child-1')}>EVENT: Child 1</li>
+        <li onClick={event => machine.send('grand-child-2-1')}>EVENT: Grand Child 2-1</li>
+        <li onClick={event => machine.send('grand-child-3-2')}>EVENT: Grand Child 3-2</li>
+        <li onClick={event => machine.send('child-4')}>EVENT: Child 4</li>
+    </ul>
     {children}
 </main>;
 
@@ -25,22 +27,22 @@ const generic = (name) => ({ children }) => <div>
 ReactDOM.render(
     <Machine id='home'>
         <State id='parent' component={App} path='/parent'>
-            <Transition event='test-event-1' target='grand-child-1-2'/>
-            <Transition event='test-event-2' target='grand-child-2-2'/>
-            <Transition event='test-event-3' target='grand-child-3-2'/>
-            <Transition event='test-event-4' target='grand-child-4-2'/>
+            <Transition event='child-1' target='child-1'/>
+            <Transition event='grand-child-2-1' target='grand-child-2-1'/>
+            <Transition event='grand-child-3-2' target='grand-child-3-2'/>
+            <Transition event='child-4' target='child-4'/>
             <State id='child-1' path='/child-1'>
                 <State id='grand-child-1' component={generic('Grand Child 1')}/>
             </State>
             <State id='child-2' path='/child-2'>
-                <State id='Grand Child 2-1' component={generic('Grand Child 2-1')}/>
+                <State id='grand-child-2-1' component={generic('Grand Child 2-1')}/>
                 <State id='grand-child-2-2' component={generic('Grand Child 2-2')}/>
             </State>
             <State id='child-3' path='/child-3' component={generic('Grand Child 3')}>
-                <State id='Grand Child 3-1' component={generic('Grand Child 3-1')}/>
+                <State id='grand-child-3-1' component={generic('Grand Child 3-1')}/>
                 <State id='grand-child-3-2' initial component={generic('Grand Child 3-2')}/>
             </State>
-            <State id='child-4' path='/child-4' component={generic('Grand Child 4')}/>
+            <State id='child-4' path='/child-4' component={generic('Child 4')}/>
         </State>
         <State id='*' component={generic('Not Found')}/>
         <State id='error' component={generic('Error')}/>
